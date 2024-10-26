ddaa284e3ed1a20d7cd713a3f1aa237008f8b540
AnuKShaju <anukshajuppr@gmail.com>
2024-10-26 19:21:21 +0530

    first commit

---
+++ javaex.java
@@ -0,0 +1,10 @@
+package com.affy;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is the change!" );
+    }
+}
